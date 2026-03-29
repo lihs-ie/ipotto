0f5a7deae9b22d8123de1bf64b28b83b3b830297
lihs-ie <lihs.dev@gmail.com>
2026-03-29 19:31:52 +0900

    Format shared HTTP bootstrap

--- services/ipo-info-fetcher/src/config.rs
+++ services/ipo-info-fetcher/src/config.rs
@@ -1,4 +1,3 @@
 use ipo_backend_shared::http::HttpServiceConfig;
 
-pub const HTTP_SERVICE_CONFIG: HttpServiceConfig =
-    HttpServiceConfig::new("ipo-info-fetcher", 8082);
+pub const HTTP_SERVICE_CONFIG: HttpServiceConfig = HttpServiceConfig::new("ipo-info-fetcher", 8082);
